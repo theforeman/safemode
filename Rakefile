theforeman/safemode

commit cb98c863800336d663938fe4a4b0112afb847b28

Release safemode 1.3.7

safemode.gemspec
@@ -2,16 +2,16 @@
 # DO NOT EDIT THIS FILE DIRECTLY
 # Instead, edit Jeweler::Tasks in Rakefile, and run 'rake gemspec'
 # -*- encoding: utf-8 -*-
-# stub: safemode 1.3.6 ruby lib
+# stub: safemode 1.3.7 ruby lib
 
 Gem::Specification.new do |s|
   s.name = "safemode".freeze
-  s.version = "1.3.6"
+  s.version = "1.3.7"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0".freeze) if s.respond_to? :required_rubygems_version=
   s.require_paths = ["lib".freeze]
   s.authors = ["Sven Fuchs".freeze, "Peter Cooper".freeze, "Matthias Viehweger".freeze, "Kingsley Hendrickse".freeze, "Ohad Levy".freeze, "Dmitri Dolguikh".freeze]
-  s.date = "2020-08-31"
+  s.date = "2022-04-26"
   s.description = "A library for safe evaluation of Ruby code based on RubyParser and Ruby2Ruby. Provides Rails ActionView template handlers for ERB and Haml.".freeze
   s.email = "ohadlevy@gmail.com".freeze
   s.extra_rdoc_files = [

commit 02ad51a0e4af2ba2a0623e95cbe10b2d139f5c9e

Drop VERSION file
Previously the version was in two places: VERSION and safemode.gemspec.
This reads the version from via Rubygems and drops the separate VERSION
file.

cb98c863800336d663938fe4a4b0112afb847b28 already forgot to update the
file and made it go out of sync.

Fixes: cb98c863800336d663938fe4a4b0112afb847b28

Rakefile
@@ -59,7 +59,7 @@ task :default => :test
 
 require 'rdoc/task'
 Rake::RDocTask.new do |rdoc|
-  version = File.exist?('VERSION') ? File.read('VERSION') : ""
+  version = Gem::Specification.find_by_name('safemode').version
 
   rdoc.rdoc_dir = 'rdoc'
   rdoc.title = "safemode #{version}"

safemode.gemspec
@@ -23,7 +23,6 @@ Gem::Specification.new do |s|
     "LICENSE",
     "README.markdown",
     "Rakefile",
-    "VERSION",
     "demo.rb",
     "init.rb",
     "lib/action_view/template_handlers/safe_erb.rb",